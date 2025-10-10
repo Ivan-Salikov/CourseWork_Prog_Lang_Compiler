ef85b735ddfb5f85fda66306a8209594d3c9f7cd
Ivan Salikov <salikovivan7@gmail.com>
2025-10-10 12:19:09 +0500

    Добавьте файлы проекта.

---
+++ CourseWork_Prog_Lang_Compiler/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CourseWork_Prog_Lang_Compiler
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
